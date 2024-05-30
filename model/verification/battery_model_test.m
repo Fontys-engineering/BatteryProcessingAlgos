1ca992ee95c0d9ca7fbf1329e25cf85f975b4ef7
Fer Clerkx <f.clerkx@student.fontys.nl>
2024-05-30 11:10:38 +0200

    Run battery_model_test multiple times with different parameters.

--- model/verification/battery_model_test.m
+++ model/verification/battery_model_test.m
@@ -2,52 +2,56 @@ clear
 clc
 
 % Specify as string to avoid precission error
-reflectionCoefsString = ["1/3" "1/10" "1"];
-delayLengths = [100 100 100];
+reflectionCoefsString = ["1/3" "1/10" "1"; "1/4" "6/7" "1";
+                         "8/10" "0.765" "1"; "0.1" "0.1" "0.1"];
+delayLengths = [100 100 100; 50 76 89; 34 35 36; 33 44 55];
 stepTime = 2.5e-12;
-stopTime = 10e-9;
+stopTime = 5e-9;
 modelName = "battery_verification_model";
 
-reflectionCoefs = arrayfun(@str2num,reflectionCoefsString);
-numImpedances = length(reflectionCoefs);
-numSteps = floorDiv(stopTime,stepTime)+1;
+numSims = size(reflectionCoefsString,1);
+for i=1:numSims
+    reflectionCoefs = arrayfun(@str2num,reflectionCoefsString(i,:));
+    numImpedances = length(reflectionCoefs);
+    numSteps = floorDiv(stopTime,stepTime)+1;
 
-simIn = Simulink.SimulationInput(modelName);
-for i=1:numImpedances
-    simIn = simIn.setBlockParameter(sprintf(modelName+"/transmit_delay%d",i),...
-                                    "DelayLength",string(delayLengths(i)));
-    simIn = simIn.setBlockParameter(sprintf(modelName+"/reflect_delay%d",i),...
-                                    "DelayLength",string(delayLengths(i)));
-end
-for i=1:numImpedances-1
-    simIn = simIn.setBlockParameter(sprintf(modelName+"/stage%d/S11",i),...
-                                    "Gain",reflectionCoefsString(i));
-    simIn = simIn.setBlockParameter(sprintf(modelName+"/stage%d/S21",i),...
-                                    "Gain","1+"+reflectionCoefsString(i));
-    simIn = simIn.setBlockParameter(sprintf(modelName+"/stage%d/S12",i),...
-                                    "Gain","1-"+reflectionCoefsString(i));
-    simIn = simIn.setBlockParameter(sprintf(modelName+"/stage%d/S22",i),...
-                                    "Gain","-"+reflectionCoefsString(i));
-end
-simIn = simIn.setBlockParameter(sprintf(modelName+"/stage%d",i+1),...
-                                "Gain",reflectionCoefsString(i+1));
-simIn = simIn.setBlockParameter(modelName+"/source",...
-                                "SampleTime",string(stepTime));
+    simIn = Simulink.SimulationInput(modelName);
+    for j=1:numImpedances
+        simIn = simIn.setBlockParameter(sprintf(modelName+"/transmit_delay%d",j),...
+                                        "DelayLength",string(delayLengths(i,j)));
+        simIn = simIn.setBlockParameter(sprintf(modelName+"/reflect_delay%d",j),...
+                                        "DelayLength",string(delayLengths(i,j)));
+    end
+    for j=1:numImpedances-1
+        simIn = simIn.setBlockParameter(sprintf(modelName+"/stage%d/S11",j),...
+                                        "Gain",reflectionCoefsString(i,j));
+        simIn = simIn.setBlockParameter(sprintf(modelName+"/stage%d/S21",j),...
+                                        "Gain","1+"+reflectionCoefsString(i,j));
+        simIn = simIn.setBlockParameter(sprintf(modelName+"/stage%d/S12",j),...
+                                        "Gain","1-"+reflectionCoefsString(i,j));
+        simIn = simIn.setBlockParameter(sprintf(modelName+"/stage%d/S22",j),...
+                                        "Gain","-"+reflectionCoefsString(i,j));
+    end
+    simIn = simIn.setBlockParameter(sprintf(modelName+"/stage%d",j+1),...
+                                    "Gain",reflectionCoefsString(i,j+1));
+    simIn = simIn.setBlockParameter(modelName+"/source",...
+                                    "SampleTime",string(stepTime));
 
-simIn = simIn.setModelParameter("FixedStep",string(stepTime));
-simIn = simIn.setModelParameter("StopTime",string(stopTime));
-out = sim(simIn);
-validationData = squeeze(out.yout{1}.Values.Data);
+    simIn = simIn.setModelParameter("FixedStep",string(stepTime));
+    simIn = simIn.setModelParameter("StopTime",string(stopTime));
+    out = sim(simIn);
+    validationData = squeeze(out.yout{1}.Values.Data);
 
-dut = battery_model("NUM_IMPEDANCES",numImpedances,"REFLECT_COEF",...
-                    reflectionCoefs,"DELAY_LENGTH",delayLengths);
-dutOut = zeros(numSteps,1);
-for i=1:numSteps
-    dutOut(i) = dut(double(i==1));
-end
+    dut = battery_model("NUM_IMPEDANCES",numImpedances,"REFLECT_COEF",...
+                        reflectionCoefs,"DELAY_LENGTH",delayLengths(i,:));
+    dutOut = zeros(numSteps,1);
+    for j=1:numSteps
+        dutOut(j) = dut(double(j==1));
+    end
 
-if isequal(dutOut,validationData)
-    disp("Pass")
-else
-    disp("Fail")
+    if isequal(dutOut,validationData)
+        disp("Pass")
+    else
+        disp("Fail")
+    end
 end
